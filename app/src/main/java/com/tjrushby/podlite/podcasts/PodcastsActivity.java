cebf80230f92655a5cda0c035c6b7bbb1fdec9f1
Tim Rushby <tjrushby@outlook.com>
2018-10-08 12:52:25 +1100

    Initial commit

---
+++ app/src/main/java/com/tjrushby/podlite/podcasts/PodcastsActivity.java
@@ -0,0 +1,20 @@
+package com.tjrushby.podlite.podcasts;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.tjrushby.podlite.R;
+
+
+public class PodcastsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_podcasts);
+
+        // subscribe to podcasts list
+    }
+
+
+}
